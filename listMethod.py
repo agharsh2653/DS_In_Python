ce27d5babded70fcd3b1983045f9dcdd4c68c28c
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-25 18:16:31 +0530

    list Method in python

---
+++ listMethod.py
@@ -0,0 +1,13 @@
+friends = ["apple", "Orange", 5, 345.06, False, "Akash", "Yashasvi"]
+print(friends[0])
+
+friends.append("Harsh")
+print(friends)
+
+li = [1,34, 62, 5, 2, 11]
+# li.sort()
+# li.reverse()
+li.insert(3,333) # insert the value 333 at index 3 in li
+value = li.pop(3)
+print(value)
+print(li)
