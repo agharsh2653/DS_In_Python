47e34d467a2021b6137ed1b379db03576c96a281
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-25 18:13:58 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,33 @@
+"""Multiline comment
+x=float("23.3")
+y=2
+print(x + y)"""
+
+#data types
+# int 
+# float 
+# booleans 
+# string
+# None
+
+   #input from user
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# marks= input("Enter your marks: ")
+# print(type(age), age)
+# print("Welcome "+name)
+# print("age =",age)
+# print("Marks =",marks)
+#
+# num1 = int(input("Enter the number1 :"))
+# num2 = int(input("Enter the number2 :"))
+# print("Sum = ",num1+num2)
+
+a= float(input("Enter first :"))
+print("%.2f"% a)
+b = float(input("Enter Second :"))
+print("Average =",(a+b)/2)
+print("Greater a",a>=b)
+
+print("Name","I","win",sep="**")
+
