bc01d0e94e13d405fdd89af3dfb6a4085cc5bf75
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-25 18:15:17 +0530

    list description

---
+++ list.py
@@ -0,0 +1,9 @@
+friends = ["apple", "Orange", 5, 345.06, False, "Akash", "Yashasvi"]
+print(friends[0])
+friends[0] = "Grapes" #unlike String, list are mutable
+print(friends[0])
+friends[2] = "lichi"
+
+#changes are occures in main list
+print(friends[1:3])
+
