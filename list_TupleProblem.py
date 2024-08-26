b32a59fb863d83802361e9fb1642dab66f085391
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-26 10:33:53 +0530

    list_tuple problem

---
+++ list_TupleProblem.py
@@ -0,0 +1,35 @@
+#Input the list of 5 fruit from user and print them
+# fruits = []
+ 
+# f1 = input("Enter the 1st name of fruit :")
+# fruits.append(f1)
+# f2 = input("Enter the 2nd name of fruit :")
+# fruits.append(f2)
+# f3 = input("Enter the 3rd name of fruit :")
+# fruits.append(f3)
+# f4= input("Enter the 4th name of fruit :")
+# fruits.append(f4) 
+# f5 = input("Enter the 5th name of fruit :")
+# fruits.append(f5)
+# print(Markss)
+
+#enter the 5 marks in the list and sort in order 
+# Marks = []
+ 
+# f1 = int(input("Enter the Marks :"))
+# Marks.append(f1)
+# f2 = int(input("Enter the Marks :"))
+# Marks.append(f2)
+# f3 = int(input("Enter the Marks :"))
+# Marks.append(f3)
+# f4= int(input("Enter the Marks :"))
+# Marks.append(f4) 
+# f5 = int(input("Enter the Marks :"))
+# Marks.append(f5)
+# f6 = int(input("Enter the Marks :"))
+# Marks.append(f6)
+# Marks.sort()
+# print(Marks)
+
+l1 = [34,56, 67, 89, 64, 90]
+print(sum(l1))
