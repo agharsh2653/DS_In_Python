4289e6ad27b90314ac8890fb97ea494fcf74d76a
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-26 20:23:09 +0530

    set description and use in python

---
+++ Set & Dictionary/set.py
@@ -0,0 +1,9 @@
+e = set() # Dont use s= {} as it will create an empty dictionary
+s = {1, 5, 32, 43, 4, 5, 2, 1, "harsh"}
+
+print(s, type(s))
+print(len(s)) #alt+shift+downword arrow
+
+s.remove(5)
+print(len(s)) #alt+shift+downword arrow
+
