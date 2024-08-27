de3f91559a531e3dff5a095e08ea4f5e763e16d1
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-28 00:24:09 +0530

    Add files via upload

---
+++ StringTest/dice.py
@@ -0,0 +1,15 @@
+import random
+print("Enter the roll for role the dice")
+print("Enter the space for exit game")
+
+while True:
+     choice = int(input())
+     if(choice==4):
+          break
+     num = random.randint(1,6)
+     print("Roll the dice number : ",num)
+     print("Enter the roll to scrol the dice: ")
+    
+          
+
+          
