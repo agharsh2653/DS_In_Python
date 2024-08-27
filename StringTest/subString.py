1395bce510058c7b77a9dc2a4540a8b6b074f1bc
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-28 00:26:55 +0530

    Add files via upload

---
+++ StringTest/subString.py
@@ -0,0 +1,9 @@
+#WAPP to check wheather a string is substring of another string or not string 
+
+str = "Pedestral Training"
+substr = "Training"
+
+if substr in str:
+     print("Yes, it is a substring.")
+else:
+     print("No, it is not a substring.")
