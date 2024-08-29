78fd339f66319b0e424c0c5f34f7a199e36a744c
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-29 21:33:34 +0530

    Add files via upload

---
+++ Loops/for_loop.py
@@ -0,0 +1,16 @@
+# for i in range(1,10):
+#      print(i)
+
+for i in range(0,20,2):
+     print(i)
+
+l = [1, "harsh", "hello", "welcome", 20.0, True, 32]
+
+# for j in range(0,len(l)):
+#      print(l[j])
+for j in l:
+     print(j)
+print()
+tup = (1,2,4,8,16,64)
+for i in tup:
+     print(i)
