546182e3745f7c8b937f0c7a32d53c2366506704
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-08-30 22:05:23 +0530

    Add files via upload

---
+++ File input & output/file_input2.py
@@ -0,0 +1,15 @@
+import os
+# f = open("myfile.txt","a")
+# f.write("Hello we are learning Python")
+# f.close()
+
+# f=open("myfile.txt","r")
+# print(f.read())
+
+
+# f = open("harsh.txt","x")
+# f.write("Hello harsh!")
+# f.close
+
+
+os.remove("harsh.txt")
