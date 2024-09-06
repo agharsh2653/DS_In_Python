65fb1574b38fc6393756dd5a0b01e6172aa46982
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-06 18:49:01 +0530

    w3resource string exercise problem44 solution

---
+++ StringPractice/Problem44.py
@@ -0,0 +1,7 @@
+#44. Write a  Python program to print the index of a character in a string.
+Sample string: w3resource
+
+string= "w3resource"
+
+for index,ch in enumerate(string):
+     print(f"Current character {ch} position at {index}")
